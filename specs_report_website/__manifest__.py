e6422b43ba2f04b5a1ecc38ba242243639f1191c
root <you@example.com>
2020-04-01 15:53:37 +0530

    view added

---
+++ specs_report_website/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Product Specs Report',
+    'category': 'Website',
+    'sequence': 100,
+    'summary': 'Get specification reports for products',
+    'website': '',
+    'version': '1.0',
+    'description': "",
+    'depends': ['website', 'sale', 'website_payment', 'website_sale ', 'website_mail', 'website_form', 'website_rating', 'digest'],
+    'data': [
+        'views/template.xml'
+    ],
+    'demo': [
+
+    ],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+}
